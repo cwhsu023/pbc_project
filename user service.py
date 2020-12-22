1af618ab12bb10d6e7171ef6b9520438343aa113
franco-1012 <75825931+franco-1012@users.noreply.github.com>
2020-12-22 15:37:26 +0800

    最新 註冊頁面有問題

--- user service.py
+++ user service.py
@@ -47,28 +47,56 @@ class Login(object):
         # 建立一個登入系統的按鈕
         self.login_button = tkinter.Button(self.root, command=self.backstage_interface, text="Login", width=10)
         # 建立一個註冊系統的按鈕
-        self.siginUp_button = tkinter.Button(self.root, command=self.siginUp_interface, text="Sign up", width=10)
+        self.siginUp_button = tkinter.Button(self.root, command= self.siginUp_interface, text="Sign up", width=10)
 
         # 完成佈局
 
     def gui_arrang(self):
+        self.label_account.pack()
+        self.input_account.pack()
+        self.login_button.pack()
+        self.siginUp_button.pack()
         self.label_account.place(x=60, y=170)
         self.input_account.place(x=135, y=170)
         self.login_button.place(x=140, y=235)
         self.siginUp_button.place(x=240, y=235)
 
-        # 進入註冊介面
 
-    def siginUp_interface(root):
-        signinWindow = tkinter.Toplevel()
+        # 進入註冊介面
+    def confirm_username(self,username):
+        confirm_window = tkinter.Toplevel()
+        confirm_window.geometry('450x300')
+        self.confirmLabel = tkinter.Label(confirm_window, text = username)
+        self.confirm_yes = tkinter.Button(confirm_window,  text="Yes !", width='10')  #command='進入遊戲畫面',
+        self.confirm_no = tkinter.Button(confirm_window, command=self.siginUp_interface, text="Nope , let's try again", width='20')
+
+        self.confirmLabel.pack()
+        self.confirm_yes.pack()
+        self.confirm_no.pack()
+        self.confirmLabel.place(x=60, y=100)
+        self.confirm_yes.place(x=70, y=170)
+        self.confirm_no.place(x=160, y=170)
+        confirm_window.mainloop()
+
+    def siginUp_interface(self):
+        signinWindow = tkinter.Toplevel(self.root)
         signinWindow.geometry('450x300')
-        root.label_username = tkinter.Label(signinWindow,text = 'username')
-        root.input_username = tkinter.Entry(signinWindow,width = 30)
-        #root.canvas1.pack()
-        root.label_username.place(x=60, y=170)
-        root.input_username.place(x=150,y=170)
+        self.label_username = tkinter.Label(signinWindow,text = 'username')
+        self.input_username = tkinter.Entry(signinWindow,width = 30)
+        username = self.input_username.get().ljust(10, " ")
+        self.confirmButton = tkinter.Button(signinWindow,command = self.confirm_username(username),text = 'Confirm')
+
+        self.label_username.pack()
+        self.input_username.pack()
+        self.confirmButton.pack()
+
+        self.label_username.place(x=60, y=170)
+        self.input_username.place(x=150,y=170)
+        self.confirmButton.place(x=120, y=200)
         signinWindow.mainloop()
+        #signinWindow.destroy()
         # 進行登入資訊驗證
+
 
 
     def backstage_interface(self):
@@ -89,7 +117,5 @@ def main():
     L.gui_arrang()
     # 主程式執行
     tkinter.mainloop()
-
-
 if __name__ == '__main__':
     main()
